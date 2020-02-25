ca34bc049d370ab62408dd53b57a14b06da735af
sergio.liberati@koedia.com <sergio.liberati@koedia.com>
2020-02-25 12:40:32 +0100

    Modified Line 23

--- canvas.js
+++ canvas.js
@@ -20,7 +20,7 @@ function animate(time) {
   ctx.fillText(FPS, 10, 10);
   ctx.beginPath();
   ctx.moveTo(xo, yo);
-  xf = xo + r * Math.cos(x);
+  //xf = xo + r * Math.cos(x);
   yf = yo + r * Math.sin(x);
   ctx.lineTo(xf, yf);
   ctx.lineTo(xf + r2 * Math.cos(-x * 2), yf + r2 * Math.sin(-x * 2));
